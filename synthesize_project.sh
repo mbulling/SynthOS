78f1e7e923695f36945afd29b622bd3fdc0a6737
Mason Bulling <mbulling@icloud.com>
2023-12-22 09:38:31 -0500

    synthesis script using config file

--- synthesize_project.sh
+++ synthesize_project.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+CONFIG_FILE="synthesis_settings.cfg"
+
+# function to read value from config file
+get_config_value() {
+    echo $(crudini --get $CONFIG_FILE "$1" "$2")
+}
+
 # prompt user to enter github repo containing c/c++ code to be synthesized
 echo "Enter the GitHub repository URL:"
 read repo_url
@@ -12,12 +19,16 @@ git clone $repo_url
 repo_name=$(basename $repo_url .git)
 cd $repo_name
 
+# read settings from config file
+VIVADO_HLS_PATH=$(get_config_value General VivadoHLSPath)
+SYNTHESIS_SCRIPT=$(get_config_value Synthesis SynthesisScript)
+SYNTHESIS_FLAGS=$(get_config_value Synthesis SynthesisFlags)
+
 # begin synthesis process
-if [ -f run_synthesis.sh ]; then
+if [ -f $SYNTHESIS_SCRIPT ]; then
     echo "starting synthesis process..."
-    chmod +x run_synthesis.sh
-    ./run_synthesis.sh
+    $VIVADO_HLS_PATH $SYNTHESIS_SCRIPT $SYNTHESIS_FLAGS
 else
-    echo "error with synthesis script"
+    echo "synthesis script not found: $SYNTHESIS_SCRIPT"
     exit 1
 fi
